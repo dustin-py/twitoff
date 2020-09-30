fd486a625b4e6be14a80d582045466f33f8bb026
dustin-py <dustinstringer92@gmail.com>
2020-09-29 20:55:18 -0700

    update app.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from .db_model import DB
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
-DB.init_app(app)
-
 def create_app():
 	"""Create and cnfigure an instance of the Flask app"""
 	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
+	DB.init_app(app)
+
 	@app.route("/")
 	def root():
 		return "Hello Twitoff"
 
 	return app
+
+#u1 = user(username='austen', followers=1500)
+#DB.session.add(u1)
+#DB.session.commit()
