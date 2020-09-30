1082764c9469f2104664ac4bbd3f8ef74139eff4
dustin-py <dustinstringer92@gmail.com>
2020-09-29 20:50:49 -0700

    added sqlite database

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from .db_model import DB
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff.db'
+DB.init_app(app)
 
 def create_app():
 	"""Create and cnfigure an instance of the Flask app"""
 	app = Flask(__name__)
-
 	@app.route("/")
 	def root():
 		return "Hello Twitoff"
