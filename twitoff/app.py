aa098e5b9de333505b97c910fe2d910ff38c4363
dustin-py <dustinstringer92@gmail.com>
2020-10-04 19:54:49 -0700

    update

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .db_model import DB, User, Tweet
-
+from .twitter import add_user_tweepy
 
 def create_app():
 	"""Create and cnfigure an instance of the Flask app"""
@@ -15,20 +15,35 @@ def create_app():
                            		users=User.query.all())
 
 
-	@app.route("/adduser/<username>/<followers>")
-	def add_user(username, followers):
-		user = User(username=username, followers=followers)
-		DB.session.add(user)
-		DB.session.commit()
-		return f"{username} has been added to the DB."
+	@app.route('/user', methods=['POST'])
+	@app.route('/user/<name>', methods=['GET'])
+	def add_or_update_user(name=None, message=''):
+		name = name or request.values['user_name']
+
+		try:
+			if request.method == "POST":
+				add_user_tweepy(name)
+				message = "User {} successfully added!".format(name)
+			tweets = User.query.filter(User.username == name).one().tweet
+		except Exception as e:
+			print(f'Error adding {name}: {e}')
+			tweets = []
+			
+		return render_template('user.html', title=name, tweets=tweets, message=message)
+	# @app.route("/adduser/<username>/<followers>")
+	# def add_user(username, followers):
+	# 	user = User(username=username, followers=followers)
+	# 	DB.session.add(user)
+	# 	DB.session.commit()
+	# 	return f"{username} has been added to the DB."
 
 
-	@app.route("/addtweet/<tweet>/<user_id>")
-	def add_tweet(tweet, user_id): 
-		string = Tweet(tweet=tweet, user_id=user_id)
-		DB.session.add(string)
-		DB.session.commit()
-		return f"{tweet}\nhas been added to the DB."
+	# @app.route("/addtweet/<tweet>/<user_id>")
+	# def add_tweet(tweet, user_id): 
+	# 	string = Tweet(tweet=tweet, user_id=user_id)
+	# 	DB.session.add(string)
+	# 	DB.session.commit()
+	# 	return f"{tweet}\nhas been added to the DB."
 	
 	return app
 
