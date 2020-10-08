ebf0f286b3a849625b33889ed347ff4d26432811
dustin-py <dustinstringer92@gmail.com>
2020-10-07 20:51:38 -0700

    update

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 """load and save spacy model"""
 import spacy
-import en_core_web_md
+import en_core_web_sm
 
-nlp = en_core_web_md.load()
+nlp = en_core_web_sm.load()
 nlp.to_disk('spacy_md_model/')
